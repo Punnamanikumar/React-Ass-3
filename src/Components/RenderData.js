f38500bcb7f1fed876fc267325b7ad5986cce605
Punnamanikumar <punnamanikumar@gmail.com>
2021-12-27 18:53:16 +0530

    Changes Made

--- src/Components/RenderData.js
+++ src/Components/RenderData.js
@@ -4,7 +4,7 @@ class RenderData extends Component {
   render() {
     return (
       <div className="Childdiv">
-        Name: {this.props.name} | Department: {this.props.dept} | Rating:{" "}
+        Name: {this.props.name} | Department: {this.props.department} | Rating:
         {this.props.rating}
       </div>
     );
